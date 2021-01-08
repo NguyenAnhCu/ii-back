c568736de5599757992202fe98e99981cb20e428
Toufiki Zakarya <zakarya.toufiki@etudiant.univ-rennes1.fr>
2021-01-08 21:13:45 +0100

    converging the code to app.py

--- app.py
+++ app.py
@@ -8,18 +8,17 @@ with warnings.catch_warnings():
 
 from marshmallow_sqlalchemy import ModelSchema
 from marshmallow import fields
-from sqlalchemy import ForeignKey
+from sqlalchemy import  ForeignKey
 from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
-app.config[
-    'SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://bd70b9c3a2566b:6cade5ff@eu-cdbr-west-03.cleardb.net/heroku_0dbbbf44bb63069'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://bd70b9c3a2566b:6cade5ff@eu-cdbr-west-03.cleardb.net/heroku_0dbbbf44bb63069'
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 90
+app.config['SQLALCHEMY_POOL_TIMEOUT'] = 20
 db = SQLAlchemy(app)
 CORS(app, support_credentials=True)
-
-
 ###Models####
-
+ 
 class User(db.Model):
     __tablename__ = "User"
     id_user = db.Column(db.Integer, primary_key=True)
@@ -27,85 +26,86 @@ class User(db.Model):
     user_score = db.Column(db.Integer)
 
     def create(self):
-        db.session.add(self)
-        db.session.commit()
-        return self
-
-    def __init__(self, user_pseudo, user_score):
+      db.session.add(self)
+      db.session.commit()
+      return self
+    
+    def __init__(self,user_pseudo, user_score):
         self.user_pseudo = user_pseudo
         self.user_score = user_score
-
+    
     def __repr__(self):
         return '' % self.id_user
-
 
 class Questions(db.Model):
     __tablename__ = "Questions"
     id_question = db.Column(db.Integer, primary_key=True)
-    is_active = db.Column(db.Enum('N', 'Y'))
+    is_active = db.Column(db.Enum('N','Y'))
 
     question_choices = relationship("QuestionChoices", backref="Questions")
 
     def create(self):
-        db.session.add(self)
-        db.session.commit()
-        return self
-
-    def __init__(self, is_active):
+      db.session.add(self)
+      db.session.commit()
+      return self
+    
+    def __init__(self,is_active):
         self.is_active = is_active
-
+    
     def __repr__(self):
         return '' % self.id_question
-
 
 class QuestionChoices(db.Model):
     __tablename__ = "Question_Choices"
     id_choice = db.Column(db.Integer, primary_key=True)
     id_question = db.Column(db.Integer, ForeignKey('Questions.id_question'))
-    is_right_choice = db.Column(db.Enum('N', 'Y'))
+    is_right_choice = db.Column(db.Enum('N','Y'))
     choice = db.Column(db.String(20))
 
     def create(self):
-        db.session.add(self)
-        db.session.commit()
-        return self
-
-    def __init__(self, is_right_choice, choice):
+      db.session.add(self)
+      db.session.commit()
+      return self
+    
+    def __init__(self,is_right_choice, choice):
         self.is_right_choice = is_right_choice
         self.choice = choice
-
+    
     def __repr__(self):
         return '' % self.id_choice
 
-
 db.create_all()
-
 
 class UserSchema(ModelSchema):
     class Meta(ModelSchema.Meta):
         model = User
         sqla_session = db.session
-
     id_user = fields.Number(dump_only=True)
     user_pseudo = fields.String(required=True)
     user_score = fields.Number(required=True)
 
-
-@app.route('/api/user/create', methods=['POST'])
+@app.route('/api/user/create', methods = ['POST'])
 @cross_origin(supports_credentials=True)
 def create_user():
     data = request.get_json()
     user_schema = UserSchema()
     user = user_schema.load(data)
     result = user_schema.dump(user.create())
-    return make_response(jsonify({"user": result}), 200)
-
+    return make_response(jsonify({"user": result}),200)
 
 # endpoint to show all users
 @app.route("/api/user", methods=["GET"])
 @cross_origin(supports_credentials=True)
 def get_users():
     get_user = User.query.all()
+    user_schema = UserSchema(many=True)
+    users = user_schema.dump(get_user)
+    return make_response(jsonify({"users": users}))
+
+# endpoint to show 5 best users score
+@app.route("/api/user/bestfive", methods=["GET"])
+def get_five_users():
+    get_user = User.query.order_by(User.user_score.desc()).limit(5).all()
     user_schema = UserSchema(many=True)
     users = user_schema.dump(get_user)
     return make_response(jsonify({"users": users}))
@@ -126,46 +126,7 @@ def delete_product_by_id(id):
     get_user = User.query.get(id)
     db.session.delete(get_user)
     db.session.commit()
-    return make_response("", 204)
-
+    return make_response("",204)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# app = Flask(__name__)
-# CORS(app)
-# api = Api(app)
-# #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@server/db'
-# #db = SQLAlchemy(app)
-#
-# questions_put_args = reqparse.RequestParser()
-# questions_put_args.add_argument("response", type=str, help="The response of the question", required=True)
-# questions_put_args.add_argument("explication", type=str, help="The description of the response", required=True)
-#
-#
-# Questions_dict = { "Questions1" : {"response": "aozheva", "explication": "aoihzbeoab"},
-#                    "Questions2" : {"response": "aedvarav", "explication": "hobaozcva"} }
-#
-# def abort_if_question_doesnt_exist(Question):
-#     if Question not in Questions_dict :
-#         abort(404, message="Question is not found")
-#
-# def abort_if_question_exist(Question):
-#     if Question in Questions_dict :
-#         abort(409, message="Question already exist")
-#
-# class Questions(Resource):
-#     def get(self, Question):
-#         abort_if_question_doesnt_exist(Question)
-#         return Questions_dict[Question]
-#
-#     def put(self, Question):
-#         abort_if_question_exist(Question)
-#         args = questions_put_args.parse_args()
-#         Questions_dict[Question] = args
-#         return Questions_dict
-#
-# api.add_resource(Questions, "/Questions/<string:Question>")
-#
-# if __name__ == "__main__" :
-#     app.run(debug=True)
